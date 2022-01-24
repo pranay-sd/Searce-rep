f66b0638cf29fe4576fff7d61dd2268accb8ac87
Pranay Jain <pranay.jain@searce.com>
2022-01-24 22:31:21 +0530

    Added assignment

---
+++ Assignment/Que1.py
@@ -0,0 +1,14 @@
+def reverseArray(a):
+    b= list()
+    
+    for i in range(len(a)):
+        b.append(a[len(a)-i-1])
+    return b
+
+if __name__ == '__main__':
+    
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
